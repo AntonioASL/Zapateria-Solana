c9867d211eb82c471429c7cf5ac1b77fb53ec7c2
Antonio Sotero <soteroloretoa08@gmail.com>
2026-03-07 23:44:18 -0600

    Update client.ts

--- client/client.ts
+++ client/client.ts
@@ -1,4 +1,61 @@
-// Client
-console.log("My address:", pg.wallet.publicKey.toString());
-const balance = await pg.connection.getBalance(pg.wallet.publicKey);
-console.log(`My balance: ${balance / web3.LAMPORTS_PER_SOL} SOL`);
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { Zapateria } from "../target/types/zapateria";
+
+async function main() {
+
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.Zapateria as Program<Zapateria>;
+
+  const owner = provider.wallet;
+
+  // PDA de la zapateria
+  const [zapateriaPDA] = anchor.web3.PublicKey.findProgramAddressSync(
+    [
+      Buffer.from("zapateria"),
+      owner.publicKey.toBuffer()
+    ],
+    program.programId
+  );
+
+  console.log("Zapateria PDA:", zapateriaPDA.toString());
+
+  // Crear Zapateria
+  await program.methods
+    .crearZapateria("Zapateria Blockchain")
+    .accounts({
+      owner: owner.publicKey,
+      zapateria: zapateriaPDA,
+      systemProgram: anchor.web3.SystemProgram.programId
+    })
+    .rpc();
+
+  console.log("Zapateria creada");
+
+  // Agregar zapato
+  await program.methods
+    .agregarZapato("Nike Air", 27, 1500)
+    .accounts({
+      owner: owner.publicKey,
+      zapateria: zapateriaPDA
+    })
+    .rpc();
+
+  console.log("Zapato agregado");
+
+  // Ver zapatos
+  await program.methods
+    .verZapatos()
+    .accounts({
+      owner: owner.publicKey,
+      zapateria: zapateriaPDA
+    })
+    .rpc();
+
+  console.log("Lista mostrada en logs");
+
+}
+
+main();
